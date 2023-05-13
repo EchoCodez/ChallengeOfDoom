0a0b8039fdfad555185b61cd5a40c08987cbcb5a
JasonGrace2282 <aarush.deshpande@gmail.com>
2023-05-12 21:03:14 -0400

    Removed unused class

--- code/log_processes/health_log.py
+++ code/log_processes/health_log.py
@@ -1,16 +1,3 @@
-'''Parse logs and put into readable format'''
+'''importable __init__.py for searching for, and rendering, log files'''
 from log_processes.get_logs import SearchForLog
 from log_processes.UI_health_log import get_previous_month
-
-class GetLogs:
-    def __init__(self, *logs) -> None:
-        self.logs = logs
-
-def main():
-    x =SearchForLog(
-        date="09/05/23"
-    ).search()
-    print(x)
-
-if __name__ == "__main__":
-    main()
