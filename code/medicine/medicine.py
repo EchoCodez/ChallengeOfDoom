ae2eef685729ec83569e3c8e96f225bd02ef0a0e
EchoCodez <mihir.nimkar@gmail.com>
2023-05-29 13:21:46 -0400

    set up medicine input

--- code/medicine/medicine.py
+++ code/medicine/medicine.py
@@ -1,18 +1,21 @@
 import customtkinter as ctk
 
 class Medicine:
-    def __init__(self, master):
+    def __init__(self, master: ctk.CTk) -> None:
         self.master = master
         self.labels = {
             0: "Medicine Name",
             1: "Morning", 
             2: "Afternoon", 
             3: "Evening",
-            4: "Before/After Meal",
+            4: "Breakfast Time",
+            5: "Lunch Time",
+            6: "Dinner Time",
+            7: "Before/After Meal",
         }
         
-    def run(self):
-        for i in range(5):
+    def run(self) -> None:
+        for i in range(8):
             ctk.CTkLabel(
                 self.master,
                 text=self.labels[i]
@@ -22,16 +25,24 @@ class Medicine:
             self.master,
             placeholder_text="Enter medicine name"
         ).grid(row=1, column=0, padx=20, pady=20)
-        '''ctk.CTkOptionMenu(
-            self.master,
-            values=["1 tablet", "2 tablets"]
-        ).grid(row=1, column=1, padx=20, pady=20)'''
+        
         for i in range(3):
             ctk.CTkEntry(
                 self.master,
                 placeholder_text="",
                 width=30
             ).grid(row=1, column=i+1, padx=20, pady=20)
+        
+        ctk.CTkOptionMenu(
+            self.master,
+            values=["Before", "After"]
+        ).grid(row=1, column=7, padx=20, pady=20)
+
+        for i in range(3):
+            ctk.CTkEntry(
+                self.master,
+                placeholder_text="",
+            ).grid(row=1, column=i+4, padx=20, pady=20)
 
         self.master.mainloop()
 
