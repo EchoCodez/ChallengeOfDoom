f98b073e34efa9dbb859fdd28659bedb9ee5b70a
EchoCodez <mihir.nimkar@gmail.com>
2023-05-29 13:03:57 -0400

    added name and dosage counter

--- code/medicine/medicine.py
+++ code/medicine/medicine.py
@@ -5,18 +5,31 @@ class Medicine:
         self.master = master
         self.labels = {
             0: "Medicine Name",
+            1: "Timing",
+            2: "Route",
+            3: "Before/After Meal",
+        }
+        self.times = {
+            0: "Morning",
             1: "Dose",
             2: "Timing",
-            3: "Route",
-            4: "Before/After Meal"
         }
         
     def run(self):
-        for i in range(5):
+        for i in range(4):
             ctk.CTkLabel(
                 self.master,
                 text=self.labels[i]
             ).grid(row=0, column=i, padx=20, pady=20)
+        
+        ctk.CTkEntry(
+            self.master,
+            placeholder_text="Enter medicine name"
+        ).grid(row=1, column=0, padx=20, pady=20)
+        ctk.CTkOptionMenu(
+            self.master,
+            values=["1 tablet", "2 tablets"]
+        ).grid(row=1, column=1, padx=20, pady=20)
 
         self.master.mainloop()
 
