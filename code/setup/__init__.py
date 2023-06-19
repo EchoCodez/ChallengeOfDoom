c0a05b775ff60b7e7f3616be1a9caff3b2513a3a
JasonGrace2282 <aarush.deshpande@gmail.com>
2023-06-19 15:11:10 -0400

    Organized setup.__init__.py

--- code/setup/__init__.py
+++ code/setup/__init__.py
@@ -1,18 +1,14 @@
 # library imports
-import tkinter as tk
-import customtkinter as ctk
-import os
-import sys
-import time
-import schedule
-import webbrowser
-from datetime import date
-from CTkMessagebox import CTkMessagebox
+from datetime import datetime, timedelta, date
 from plyer import notification
-from datetime import datetime
-from datetime import timedelta
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
+from CTkMessagebox import CTkMessagebox
+import tkinter as tk
+import customtkinter as ctk
+import sys, os
+import webbrowser
+import time, schedule
 
 # folder imports
 from utils import *
