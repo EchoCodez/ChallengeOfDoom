2f6a6354b9f5622b22b79c01cc4f9d1bc9c99488
EchoCodez <mihir.nimkar@gmail.com>
2023-06-20 00:46:23 -0400

    modified destroy mechanism and added log title

--- code/health/health_log.py
+++ code/health/health_log.py
@@ -16,6 +16,7 @@ class Calendar:
             6: "Saturday"
         }
         self.logger = self.master.logger
+        self.days = []
     
     def run(self, mainloop: bool = True) -> None:
         today = datetime.today()
@@ -28,21 +29,20 @@ class Calendar:
             ).grid(row=0, column=i, padx=20, pady=20)
             
         week = 1
-        days = []
         for num in range(1, month+1):
             num += offset
-            day = Day(num, offset, self.master)
+            day = Day(num, offset, self.master,  f"{today.month}/{num-offset}/{today.year}", self)
             day.grid(row=week, column=num%7, padx=5, pady=5)
-            days.append(day)
+            self.days.append(day)
             if (num+1)%7==0:
                 week+=1
         
-        self.logger.debug([element._text for element in days])
+        self.logger.debug([element._text for element in self.days])
         if mainloop:
             self.master.mainloop()
 
 class Day(ctk.CTkButton):
-    def __init__(self, num: int, offset: int, master: ctk.CTk) -> None:
+    def __init__(self, num: int, offset: int, master: ctk.CTk, fulldate: str, calendar: Calendar) -> None:
         super().__init__(
                 master, 
                 text=f"{num-offset}",
@@ -51,16 +51,21 @@ class Day(ctk.CTkButton):
                 fg_color=None,
                 command=self.open_log
                 )
-        self.log = None
+        self.log, self.win = None
+        self.fulldate = fulldate
+        self.calendar = calendar
 
     def open_log(self):
-        win = Log()
+        for day in self.calendar.days:
+            if day != self and day.win:
+                day.win.destroy()
+        self.win = Log(self.master, self.fulldate)
     
 class Log(ctk.CTkToplevel):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, master: ctk.CTk, title: str) -> None:
+        super().__init__(master)
         self.geometry("400x300")
-
+        super(Log, self).title(title)
         self.label = ctk.CTkLabel(self, text=f"")
         self.label.pack(padx=20, pady=20)
 
