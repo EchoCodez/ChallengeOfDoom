e5f7addea90062b6a76244de573ac8d206442766
JasonGrace2282 <aarush.deshpande@gmail.com>
2023-07-24 16:44:19 -0400

    made pollen stuff write to file

--- code/api/forecast.py
+++ code/api/forecast.py
@@ -1,15 +1,32 @@
 import requests
+import json
+import ast
 
-result = requests.get('https://api.ambeedata.com/latest/pollen/by-lat-lng?lat=37.54068&lng=-77.43367',
-             headers = {"x-api-key": "4fdc231b972e03b1df86d489299f2c39c62375be477304012f912fae0039a3a8", "Content-type": "application/json"}).content
-print('Grass Pollen Count: ' + str(eval(result)['data'][0]['Count']['grass_pollen']))
-print('Tree Pollen Count: ' + str(eval(result)['data'][0]['Count']['tree_pollen']))
-print('Weed Pollen Count: ' + str(eval(result)['data'][0]['Count']['weed_pollen']))
-print('Grass Pollen Risk: ' + str(eval(result)['data'][0]['Risk']['grass_pollen']))
-print('Tree Pollen Risk: ' + str(eval(result)['data'][0]['Risk']['tree_pollen']))
-print('Weed Pollen Risk: ' + str(eval(result)['data'][0]['Risk']['weed_pollen']))
+# from utils import WeatherInfo, UseLogger
 
-[{
-    'Count': {'grass_pollen': 35, 'tree_pollen': 0, 'weed_pollen': 88}, 
-  'Risk': {'grass_pollen': 'Moderate', 'tree_pollen': 'Low', 'weed_pollen': 'High'}, 
-  'updatedAt': '2023-07-12T23:11:31.000Z'}]
+
+# class WeatherData(UseLogger):
+class WeatherData:
+    def make_call(self):
+        url = 'https://api.ambeedata.com/latest/pollen/by-lat-lng?lat=37.54068&lng=-77.43367'
+        result = requests.get(
+            url,
+            headers = {
+                "x-api-key": "4fdc231b972e03b1df86d489299f2c39c62375be477304012f912fae0039a3a8",
+                "Content-type": "application/json"
+                }
+        ).json()
+        
+        with open("json/pollen.json", "w") as f:
+            f.write(json.dumps(result, indent=4))
+    
+
+            
+        # print('Grass Pollen Count: ' + str(eval(result)['data'][0]['Count']['grass_pollen']))
+        # print('Tree Pollen Count: ' + str(eval(result)['data'][0]['Count']['tree_pollen']))
+        # print('Weed Pollen Count: ' + str(eval(result)['data'][0]['Count']['weed_pollen']))
+        # print('Grass Pollen Risk: ' + str(eval(result)['data'][0]['Risk']['grass_pollen']))
+        # print('Tree Pollen Risk: ' + str(eval(result)['data'][0]['Risk']['tree_pollen']))
+        # print('Weed Pollen Risk: ' + str(eval(result)['data'][0]['Risk']['weed_pollen']))
+        
+print(WeatherData().make_call())
