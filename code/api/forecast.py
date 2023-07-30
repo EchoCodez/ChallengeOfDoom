060e80923557e6976192cfff28dcd2a1c4b2f18c
JasonGrace2282 <aarush.deshpande@gmail.com>
2023-07-30 07:25:35 -0400

    Fixed crash

--- code/api/forecast.py
+++ code/api/forecast.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import ast
 
 # from utils import WeatherInfo, UseLogger
 
@@ -10,12 +9,17 @@ class WeatherData:
     def make_call(self):
         url = 'https://api.ambeedata.com/latest/pollen/by-lat-lng?lat=37.54068&lng=-77.43367'
         result = requests.get(
-            url,
-            headers = {
-                "x-api-key": "4fdc231b972e03b1df86d489299f2c39c62375be477304012f912fae0039a3a8",
-                "Content-type": "application/json"
-                }
-        ).json()
+                url,
+                headers = {
+                    "x-api-key": "4fdc231b972e03b1df86d489299f2c39c62375be477304012f912fae0039a3a8",
+                    "Content-type": "application/json"
+                    }
+            )
+        try:
+            result.json()
+        except requests.exceptions.JSONDecodeError as e:
+            print(result.content)
+            exit(0)
         
         with open("json/pollen.json", "w") as f:
             f.write(json.dumps(result, indent=4))
@@ -28,5 +32,6 @@ class WeatherData:
         # print('Grass Pollen Risk: ' + str(eval(result)['data'][0]['Risk']['grass_pollen']))
         # print('Tree Pollen Risk: ' + str(eval(result)['data'][0]['Risk']['tree_pollen']))
         # print('Weed Pollen Risk: ' + str(eval(result)['data'][0]['Risk']['weed_pollen']))
-        
-print(WeatherData().make_call())
+
+if __name__ == "__main__":
+    print(WeatherData().make_call())
