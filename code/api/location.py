4e1a20ce8357ed69e0d254b32d9d66200bec8378
JasonGrace2282 <aarush.deshpande@gmail.com>
2023-07-30 13:46:24 -0400

    added description to api.location.get_location

--- code/api/location.py
+++ code/api/location.py
@@ -2,6 +2,14 @@ from geopy.geocoders import Nominatim
 from typing import Coroutine
 
 def get_location(geocode: str) -> Coroutine:
+    '''Return geocode object based on geocode.
+    
+    Args:
+    -----
+        geocode (str): location.
+            Ex) Ashburn Virginia'''
+    # https://www.geeksforgeeks.org/how-to-get-geolocation-in-python/
+    # method 1
     return Nominatim(user_agent="GetLoc").geocode(geocode)
 
 if __name__ == "__main__":
