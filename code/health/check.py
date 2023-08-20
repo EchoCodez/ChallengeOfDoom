4c41c81c80a4eb79d31fa32cae982b1674789041
EchoCodez <mihir.nimkar@gmail.com>
2023-08-20 19:03:00 -0400

    scaled quantities

--- code/health/check.py
+++ code/health/check.py
@@ -30,36 +30,44 @@ class Algorithm:
         nutrients = {}
         for detail in food_details:
             if detail["nutrient"]["name"] == "Energy":
-                nutrients["Calories"] = [float(detail["amount"]), detail["nutrient"]["unitName"]]
+                nutrients["Calories"] = [float(detail["amount"])*self.quantity/100, detail["nutrient"]["unitName"]]
             if detail["nutrient"]["name"] == "Protein":
-                nutrients["Protein"] = [float(detail["amount"]), detail["nutrient"]["unitName"]]
+                nutrients["Protein"] = [float(detail["amount"]*self.quantity/100), detail["nutrient"]["unitName"]]
             if detail["nutrient"]["name"] == "Sugars, total including NLEA":
-                nutrients["Sugar"] = [float(detail["amount"]), detail["nutrient"]["unitName"]]
+                nutrients["Sugar"] = [float(detail["amount"]*self.quantity/100), detail["nutrient"]["unitName"]]
             if detail["nutrient"]["name"] == "Calcium, Ca":
-                nutrients["Calcium"] = [float(detail["amount"]), detail["nutrient"]["unitName"]]
+                nutrients["Calcium"] = [float(detail["amount"]*self.quantity/100), detail["nutrient"]["unitName"]]
             if detail["nutrient"]["name"] == "Iron, Fe":
-                nutrients["Iron"] = [float(detail["amount"]), detail["nutrient"]["unitName"]]
+                nutrients["Iron"] = [float(detail["amount"]*self.quantity/100), detail["nutrient"]["unitName"]]
             if detail["nutrient"]["name"] == "Fiber, total dietary":
-                nutrients["Fiber"] = [float(detail["amount"]), detail["nutrient"]["unitName"]]
+                nutrients["Fiber"] = [float(detail["amount"]*self.quantity/100), detail["nutrient"]["unitName"]]
             if detail["nutrient"]["name"] == "Cholesterol":
-                nutrients["Cholesterol"] = [float(detail["amount"]), detail["nutrient"]["unitName"]]
+                nutrients["Cholesterol"] = [float(detail["amount"]*self.quantity/100), detail["nutrient"]["unitName"]]
             if detail["nutrient"]["name"] == "Carbohydrate, by difference":
-                nutrients["Carbs"] = [float(detail["amount"]), detail["nutrient"]["unitName"]]
+                nutrients["Carbs"] = [float(detail["amount"]*self.quantity/100), detail["nutrient"]["unitName"]]
             if detail["nutrient"]["name"] == "Potassium, K":
-                nutrients["Potassium"] = [float(detail["amount"]), detail["nutrient"]["unitName"]]
+                nutrients["Potassium"] = [float(detail["amount"]*self.quantity/100), detail["nutrient"]["unitName"]]
             if detail["nutrient"]["name"] == "Total lipid (fat)":
-                nutrients["Fat"] = [float(detail["amount"]), detail["nutrient"]["unitName"]]
+                nutrients["Fat"] = [float(detail["amount"]*self.quantity/100), detail["nutrient"]["unitName"]]
             if detail["nutrient"]["name"] == "Sodium, Na":
-                nutrients["Sodium"] = [float(detail["amount"]), detail["nutrient"]["unitName"]]
+                nutrients["Sodium"] = [float(detail["amount"]*self.quantity/100), detail["nutrient"]["unitName"]]
             if detail["nutrient"]["name"] == "Vitamin C, total ascorbic acid":
-                nutrients["Vitamin C"] = [float(detail["amount"]), detail["nutrient"]["unitName"]]
+                nutrients["Vitamin C"] = [float(detail["amount"]*self.quantity/100), detail["nutrient"]["unitName"]]
             if detail["nutrient"]["name"] == "Vitamin A, IU":
-                nutrients["Vitamin D"] = [float(detail["amount"]), detail["nutrient"]["unitName"]]
+                nutrients["Vitamin D"] = [float(detail["amount"]*self.quantity/100), detail["nutrient"]["unitName"]]
             if detail["nutrient"]["name"] == "Vitamin D (D2 + D3), International Units":
-                nutrients["Vitamin D"] = [float(detail["amount"]), detail["nutrient"]["unitName"]]
-
+                nutrients["Vitamin D"] = [float(detail["amount"]*self.quantity/100), detail["nutrient"]["unitName"]]
+        with open("json/food.json", "r") as f:
+            try:
+                contents = json.load(f)
+                empty = False
+            except:
+                empty = True
         with open("json/food.json", "w") as f:
-            json.dump(nutrients, f, indent=4)
+            if not empty:
+                contents["Calories"][0] += nutrients["Calories"][0]
+            else:
+                json.dump(nutrients, f, indent=4)
         return raw_data["foodNutrients"]
 
     def run(self):
